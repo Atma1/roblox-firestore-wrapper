d45c93cdb3428243ce8aa9fd83be049015618580
Atma1 <atmasusanto08@gmail.com>
2021-05-24 17:43:45 +0800

    severly updated the api wrapper

--- firestore/src/ServerScriptService/Folder/FireStoreWrapper.lua
+++ firestore/src/ServerScriptService/Folder/FireStoreWrapper.lua
@@ -21,7 +21,7 @@ function fireStore:setDataBaseSecrets(
 end
 
 -- Create a request object
-function fireStore:createRequest(endPoint:string, method:string)
+function fireStore:createRequestObject(endPoint:string, method:string)
 
 	local request = {
 		Url = endPoint,
@@ -34,93 +34,6 @@ function fireStore:createRequest(endPoint:string, method:string)
 	return request
 end
 
--- grab the whole collection from DB
-function fireStore:getFireStore(collectionPath:string)
-
-local request = self:createRequest(self.dataBaseLink..collectionPath, 'GET')
-
-	return Promise.new(function(resolve, reject)
-		local response = https.request(request.Method, request.Url, {
-			headers=request.Headers
-		})
-
-		if response.ok then
-			local collection = response:json()
-			resolve(collection)
-		else
-			reject(response.status_code, response.message)
-		end
-	end)
-end
-
--- Get document
-
-function fireStore:getDocument(documentPath:string)
-	local request = self:createRequest(self.dataBaseLink..documentPath, 'GET')
-
-	return Promise.new(function(resolve, reject)
-		local response = https.request(request.Method, request.Url, {
-			headers=request.Headers
-		})
-
-		if response.ok then
-			resolve(response:json(response['Body']))
-		else
-			reject(response.status_code, response)
-		end
-	end)
-end
-
--- create document. docname is optional and if left out firestore will create generate random id for the doc name
-function fireStore:createDocument(documentCollectionId:string, updatedDocument, documentName:string)
-	local request = self:createRequest(self.dataBaseLink..documentCollectionId, 'POST')
-
-	return Promise.new(function(resolve, reject)
-		local response = https.request(request.Method, request.Url, {
-			headers=request.Headers, data=updatedDocument, query = documentName or nil
-		})
-
-		if response.ok then
-			resolve(response.status_code)
-		else
-			reject(response.status_code, response.messsage)
-		end
-	end)
-end
-
--- Delete document
-function fireStore:deleteDocument(documentPath:string)
-
-	local request = self:createRequest(self.dataBaseLink..documentPath, 'DELETE')
-
-	return Promise.new(function(resolve, reject)
-		local response = https.request(request.Method, request.Url, {
-			headers = request.Headers
-		})
-
-		if response.ok then
-			resolve(response.status_code)
-		else
-			reject(response.status_code, response)
-		end
-	end)
-end
-
-function fireStore:updateDocument(documentPath:string, query)
-	local request = self:createRequest(self.dataBaseLink..documentPath, 'PATCH')
-
-	return Promise.new(function(resolve, reject)
-		local response = https.request(request.Method, request.Url, {
-			headers=request.Headers, query = query or {}
-		})
-		if response.ok then
-			resolve(response)
-		else
-			reject(response.status_code, response)
-		end
-	end)
-end
-
 function fireStore:updatedataBaseIdToken(updatedToken:string)
 	self.dataBaseIdToken = updatedToken
 	print('dataBaseAuthToken has been updated')
@@ -129,11 +42,11 @@ end
 -- Sign in to FB Auth anonymously. result from request is idToken, refreshToken, token expiresIn
 function fireStore:anonymousSignIn()
 
-	local request = self:createRequest(self.anonymousSignInLink, 'POST')
+	local request = self:createRequestObject(self.anonymousSignInLink, 'POST')
 
 	return Promise.new(function(resolve, reject)
 		local response = https.request(request.Method, request.Url, {
-			query={ key = self.webApiKey}
+			query={ key=self.webApiKey}
 		})
 
 		if response.ok then
@@ -144,7 +57,7 @@ function fireStore:anonymousSignIn()
 	end)
 end
 
--- exchange refresh token to refresh id token when it expires
+-- exchange refresh token to refresh id token. used when it expires
 function fireStore:exchangeRefreshTokenForAnIdToken(refreshToken:string)
 
 	local payload = {
@@ -163,11 +76,115 @@ function fireStore:exchangeRefreshTokenForAnIdToken(refreshToken:string)
 			local decodedResponse = response:json()
 			local idToken = decodedResponse['id_token']
 			self:updatedataBaseIdToken(idToken)
-			resolve()
+			resolve(response)
 		else
 			reject(response.status_code, response.message)
 		end
 	end)
 end
 
+-- grab the whole collection from DB
+function fireStore:getColletion(collectionPath:string)
+
+	local url = self.dataBaseLink..collectionPath
+	local collection = {}
+
+		function collection:Request(method:string, docPath:string, requestBody)
+			local request = fireStore:createRequestObject(url..docPath, method)
+
+			return Promise.new(function(resolve, reject)
+				local response = https.request(request.Method, request.Url, {
+						headers=request.Headers, data=requestBody or {}
+				})
+				if response.ok then
+						resolve(response:json(response['Body']))
+				else
+					reject(response.statu_code, response)
+				end
+			end)
+		end
+
+
+		-- Get document
+		-- mask is the field of the doc to return. if not specified fireStore will return all doc's fields
+		function collection:getDocument(documentPath:string, mask:string)
+			local request = fireStore:createRequestObject(url..documentPath, 'GET')
+
+			return Promise.new(function(resolve, reject)
+				local response = https.request(request.Method, request.Url, {
+					headers=request.Headers, query = mask or {}
+				})
+
+				if response.ok then
+					resolve(response:json(response['Body']))
+				else
+					reject(response)
+				end
+			end)
+		end
+
+		-- create document. docname is optional and if left out fireStore will create generate random id for the doc name
+		-- mask is the field of the doc to return. if not specified fireStore will return all doc's fields
+		function collection:createDocument(documentInstance, documentName:string, mask:string)
+			assert(documentInstance ~=nil, 'The document must not be empty.')
+
+			local request = fireStore:createRequestObject(url, 'POST')
+
+			return Promise.new(function(resolve, reject)
+				local response = https.request(request.Method, request.Url, {
+					headers=request.Headers, data=documentInstance, query = {
+						documentId = documentName or {}, mask = mask or {}
+					}
+				})
+
+				if response.ok then
+					resolve(response)
+				else
+					reject(response)
+				end
+			end)
+		end
+
+		-- Delete document
+		function collection:deleteDocument(documentPath:string)
+
+			local request = fireStore:createRequestObject(url..documentPath, 'DELETE')
+
+			return Promise.new(function(resolve, reject)
+				local response = https.request(request.Method, request.Url, {
+					headers = request.Headers
+				})
+
+				if response.ok then
+					resolve(response.status_code)
+				else
+					reject(response)
+				end
+			end)
+		end
+
+		--updatemask is location of the field to be updated.
+		function collection:patchDocument(documentPath:string, updatedDoc, updateMask:string, currentDocexists:boolean, mask:string)
+			assert(updateMask ~= nil, 'The update mask must not be empty.')
+			assert(updatedDoc ~= nil, 'Updated doc must not be empty')
+
+			local request = fireStore:createRequestObject(url..documentPath, 'PATCH')
+
+			return Promise.new(function(resolve, reject)
+				local response = https.request(request.Method, request.Url, {
+					headers=request.Headers, data=updatedDoc, query ={
+						['updateMask.fieldPaths']=updateMask, ['currentDocument.exists']=currentDocexists or {},
+						mask=mask or {}
+					}
+				})
+
+				if response.ok then
+					resolve(response:json())
+				else
+					reject(response)
+				end
+			end)
+		end
+	return collection
+	end
 return fireStore

e00365986d13b41ac0f60b1e15a8c172292dde81
Atma1 <atmasusanto08@gmail.com>
2021-07-07 11:25:52 +0800

    minor change

--- firestore/FireStoreWrapper2ElectricBoogalo.lua
+++ firestore/FireStoreWrapper2ElectricBoogalo.lua
@@ -1,24 +1,23 @@
 local serverScriptService = game:GetService('ServerScriptService')
 local Promise = require(serverScriptService.fireStore.lib.Promise)
 local https = require(serverScriptService.fireStore.lib.http)
-local fireStore = {}
-fireStore.__index = fireStore
+local firestore = {}
+firestore.__index = firestore
 local collection = {}
 collection.__index = collection
 
-function fireStore.new(dataBaseLink:string, dataBaseIdToken:string , fireBaseWebApiKey:string, refreshToken:string)
+function firestore.new(dataBaseLink:string, dataBaseIdToken:string , fireBaseWebApiKey:string, refreshToken:string)
     local self = {}
-    setmetatable(self, fireStore)
-    self.dataBaseLink = dataBaseLink
+	self.dataBaseLink = dataBaseLink
     self.dataBaseIdToken = dataBaseIdToken
     self.fireBaseWebApiKey = fireBaseWebApiKey
     self.fireStoreAuthRefreshToken = refreshToken
     self.refreshTokenLink = 'https://securetoken.googleapis.com/v1/token'
     self.anonymousSignInLink = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp'
-    return self
+    return setmetatable(self, firestore)
 end
 
-function fireStore:newRequestObject(endPoint:string, method:string)
+function firestore:newRequestObject(endPoint:string, method:string)
     local request = {
 		Url = endPoint,
 		Method = method,
@@ -30,12 +29,12 @@ function fireStore:newRequestObject(endPoint:string, method:string)
 	return request
 end
 
-function fireStore:updatedataBaseIdToken(updatedToken:string)
+function firestore:updatedataBaseIdToken(updatedToken:string)
     self.dataBaseIdToken = updatedToken
     print('dataBaseAuthToken has been updated')
 end
 
-function fireStore:anonymousSignIn()
+function firestore:anonymousSignIn()
 
 	local request = self:newRequestObject(self.anonymousSignInLink, 'POST')
 
@@ -52,9 +51,9 @@ function fireStore:anonymousSignIn()
 	end)
 end
 
-function fireStore:refreshIdToken(refreshToken:string)
+function firestore:refreshIdToken(refreshToken:string)
 
-	local payload = {
+	local data = {
 		grant_type = "refresh_token",
 		refresh_token = refreshToken
 	}
@@ -63,7 +62,7 @@ function fireStore:refreshIdToken(refreshToken:string)
 
 	return Promise.new(function(resolve, reject)
 		local response = https.request('POST', self.refreshTokenLink, {
-			data=payload, headers=requestHeader, query=requestQuery,
+			data=data, headers=requestHeader, query=requestQuery,
 		})
 
 		if response.ok then
@@ -74,15 +73,14 @@ function fireStore:refreshIdToken(refreshToken:string)
 	end)
 end
 
-function fireStore:Collection(collectionPath:string)
+function firestore:Collection(collectionPath:string)
 	local fireStoreCollection = {}
-    setmetatable(fireStoreCollection, collection)
 	fireStoreCollection.collectionPathUrl = collectionPath
-    return collection
+    return setmetatable(fireStoreCollection, collection)
 end
 
-function collection:Request(method:string, url:string, searchQuery, requestBody)
-	local request = fireStore:newRequestObject(self.collectionPathUrl..url, method)
+function collection:request(method:string, url:string, searchQuery:table, requestBody:table)
+	local request = firestore:newRequestObject(url, method)
 
 	return Promise.new(function(resolve, reject)
 		local response = https.request(request.Method, request.Url, {
@@ -97,40 +95,40 @@ function collection:Request(method:string, url:string, searchQuery, requestBody)
 end
 
 function collection:getAllDocuments()
-	local request = fireStore:newRequestObject(self.collectionPathUrl, 'GET')
-	return self:Request(request.Method)
+	local request = firestore:newRequestObject(self.collectionPathUrl, 'GET')
+	return self:request(request.Method)
 end
 
 function collection:getDocument(documentPath:string, mask:string)
-	local request = fireStore:newRequestObject(self.collectionPathUrl..documentPath, 'GET')
-	return self:Request(request.Method, documentPath, mask or {})
+	local request = firestore:newRequestObject(self.collectionPathUrl..documentPath, 'GET')
+	return self:request(request.Method, request.Url, mask or {})
 end
 
-function collection:createDocument(document, documentName:string, mask:string)
+function collection:createDocument(document:table, documentName:string, mask:string)
 	assert(document ~=nil, 'The document must not be empty.')
 
 	local query = {
-		documentName = documentName or nil,
-		mask = mask or nil
+		documentName = documentName or {},
+		mask = mask or {}
 	}
-	local request = fireStore:createRequestObject(self.collectionPathUrl, 'POST')
-	return self:Request(request.Method, request.Url, query, document)
+	local request = firestore:createRequestObject(self.collectionPathUrl, 'POST')
+	return self:request(request.Method, request.Url, query, document)
 end
 
 function collection:deleteDocument(documentPath:string)
-	local request = fireStore:createRequestObject(self.collectionPathUrl..documentPath, 'DELETE')
-	return self:Request(request.Method, request.Url)
+	local request = firestore:createRequestObject(self.collectionPathUrl..documentPath, 'DELETE')
+	return self:request(request.Method, request.Url)
 end
 
-function collection:patchDocument(documentPath:string, updatedDoc, updateMask:string, docExist:boolean, mask:string)
+function collection:patchDocument(documentPath:string, updatedDoc:table, updateMask:string, docExist:boolean, mask:string)
 	assert(updatedDoc ~=nil, 'The document must not be empty.')
 	assert(updateMask ~= nil, 'Update mask must not be empty')
-	assert(updateMask ~= nil, 'docExist must not be empty')
+	assert(docExist ~= nil, 'docExist must not be empty')
 	local query = {
-		mask = mask or nil,
-		['updateMask.fieldPaths']=updateMask,
-		['currentDocument.exists']=docExist or {}
+		mask = mask or {},
+		['updateMask.fieldPaths'] = updateMask,
+		['currentDocument.exists'] = docExist or {}
 	}
-	local request = fireStore:createRequestObject(self.collectionPathUrl..documentPath, 'POST')
-	return self:Request(request.Method, request.Url, query, updatedDoc)
+	local request = firestore:createRequestObject(self.collectionPathUrl..documentPath, 'POST')
+	return self:request(request.Method, request.Url, query, updatedDoc)
 end
